dc698c6a1d1d4a39a40bd3c1eb12f913a8c1b5a5
PASALAUDAY <162982683+PASALAUDAY@users.noreply.github.com>
2024-03-11 16:10:21 +0530

    Add files via upload

---
+++ file1.py
@@ -0,0 +1,81 @@
+
+'''
+ Bargained characters
+
+  Controls and data structure-->ABC LANGUAGE
+  Strings and its format-->from C
+  Regular expressions-->Pearl
+  Exception handling-->Module 3
+  Magic methods from-->fortron
+
+  #print("hello")
+
+
+print("hello world")
+print("hello world")
+print("hello world")
+
+1).Commenting is used for making documentation in a python file.
+2).When the line is commented , that line will not be considered as a part of the code.
+3).When the program is exucuted, commented line will not be in the part of execution.
+
+------> NOTE
+Single line commenting can be done using #
+
+#print("hello world")
+#print("hello world")
+#import this
+#print(this)
+
+
+# import is a keybord which is used to communicated between the python files.
+import antigravity
+
+day.1
+ Bargained characters
+
+   controls and Data structures > ABC language
+   Strings and its formats > from C
+   Regular expressions >Pearl
+   Exception handling > Module 3
+   Magic methods from > fortron
+
+ 
+ Flavours of Python
+
+  > Cpython-Written in C,Most common implemwntation of python.
+  > Jython-Java implementation of python.
+  > Ironpython-implementation of the Python programming language which is tightly integrated with,NET.
+  > Brython- Browser Python,runs in browsers
+  > Rubypython- Bridge between python and Ruby Interpreters
+  > MicroPython- Runs in Micro Controllers
+
+  # ---> print(object,sep, end,file, flush)
+  # print has 5 parameters
+
+  # object --->the value to be printed is objcet
+  # sep---> seperator
+  # Eg
+  # a=8
+  # b=9
+  # c=89
+  # print(a,b,c  sep='$')
+
+
+# end--> which is used to add the string at the end of the Value,
+# It will not keep the command to stay in same line
+#
+#Eg:
+#print(str(a), b, end='%%')
+#print(str(c))
+
+#!----> variables
+# ?variable is a name which is used to identify the value of variable # ?which is stored in a specific memory location
+#! Rules to dedfine variables
+# 1.) Variable have to start with only alphabets, underscores
+# 2.) The char following the 1st char can be alphabets, numbers, underscores
+# 3.) No spaces are allowed
+# 4.) No special characters like !@#$%^&*()+
+#5.) case sensitive
+# 6.) Keywords are not allowed as variables
+'''
